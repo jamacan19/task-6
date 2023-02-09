55f61780b99f1069d1d3d902843c8bba10ece052
jamacan19 <124731983+jamacan19@users.noreply.github.com>
2023-02-09 18:16:24 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ basic.forever(function () {
     if (input.lightLevel() < 100) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, randint(0, 50))
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, randint(0, 50))
+        basic.pause(200)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
